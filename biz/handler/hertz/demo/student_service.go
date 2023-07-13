4230bd4a8a6b04870403b7f65f16d806fd99572a
Begonia <2239849100@qq.com>
2023-07-13 13:02:33 +0800

    student Test OK

--- biz/handler/hertz/demo/student_service.go
+++ biz/handler/hertz/demo/student_service.go
@@ -5,9 +5,16 @@ package demo
 import (
 	"context"
 
-	"github.com/cloudwego/hertz/pkg/app"
-	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	demo "project2/biz/model/hertz/demo"
+	// kitexdemo "project2/kitex_gen/hertz/demo"
+	// studentservice "project2/kitex_gen/hertz/demo/studentservice"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/adaptor"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	kclient "github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/client/genericclient"
+	"github.com/cloudwego/kitex/pkg/generic"
 )
 
 // Register .
@@ -15,11 +22,35 @@ import (
 func Register(ctx context.Context, c *app.RequestContext) {
 	var err error
 	var req demo.Student
+
 	err = c.BindAndValidate(&req)
 	if err != nil {
 		c.String(consts.StatusBadRequest, err.Error())
 		return
 	}
+
+
+	// httpReq, errhttp := adaptor.GetCompatRequest(c.GetRequest())
+	// if errhttp != nil {
+	// 	c.String(consts.StatusBadRequest, errhttp.Error())
+	// }
+	// cli, errhitex := studentservice.NewClient("register", client.WithHostPorts("127.0.0.1:9999"))
+	// if errhitex != nil {
+	// 	c.String(consts.StatusBadRequest, errhitex.Error())
+	// }
+
+	// RegiReq := &kitexdemo.Student{
+	// 	Id:   req.ID,
+	// 	Name: req.Name,
+	// 	College: &kitexdemo.College{
+	// 		Name:    req.College.Name,
+	// 		Address: req.College.Address,
+	// 	},
+	// 	Email: req.Email,
+	// }
+	// RegiResp, respError := cli.Register(context.Background())
+
+
 
 	resp := new(demo.RegisterResp)
 
@@ -37,7 +68,45 @@ func Query(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	resp := new(demo.Student)
+	cli := initGenericClient()
+
+	httpReq, err := adaptor.GetCompatRequest(c.GetRequest())
+	if err!=nil{
+		panic("get http req failed")
+	}
+
+	customReq, err := generic.FromHTTPRequest(httpReq)
+	if err!=nil{
+		panic("get custom req failed")
+	}
+
+	resp, err := cli.GenericCall(ctx,"Query",customReq)
+
+
+	// resp := new(demo.Student)
 
 	c.JSON(consts.StatusOK, resp)
+}
+
+
+func initGenericClient() genericclient.Client{
+	p,err:=generic.NewThriftFileProvider("./idl/student.thrift")
+	if err!=nil{
+		panic(err)
+	}
+
+	g,err:=generic.HTTPThriftGeneric(p)
+	if err!=nil{
+		panic(err)
+	}
+
+	cli,err := genericclient.NewClient("Register", g ,
+		kclient.WithHostPorts("127.0.0.1:9999"),
+	)
+
+	if err!=nil{
+		panic(err)
+	}
+
+	return cli
 }
